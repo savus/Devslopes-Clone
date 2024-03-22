0e354a53c13544fe8b16635c777eb8c9509f8d87
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2024-03-22 09:10:10 -0600

    begin adding base css styles

---
+++ src/Components/MainSection.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export const MainSection = ({ children }: { children: ReactNode }) => {
+  return <div className="main-section">{children}</div>;
+};
---
+++ src/Components/TopHeader.tsx
@@ -0,0 +1,25 @@
+export const TopHeader = () => {
+  return (
+    <div className="top-header">
+      <div className="top-navigation">
+        <div className="header-logo">DEVSLOPES</div>
+        <nav className="header-navbar">
+          <ul>
+            <li>
+              <a href="#">Plans</a>
+            </li>
+            <li>
+              <a href="#">Youtube</a>
+            </li>
+            <li>
+              <a href="#">Instagram</a>
+            </li>
+            <li>
+              <a href="#">Sign In</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
