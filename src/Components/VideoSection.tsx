d10ec65e388f1e60678991cb1296d55f32f57c41
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2024-03-25 14:11:07 -0600

    finish up styling VideoSection component

--- src/Components/VideoSection.tsx
+++ src/Components/VideoSection.tsx
@@ -3,9 +3,13 @@ export const VideoSection = () => {
   return (
     <section className="video-section">
       <div className="video-container">
-        <button className="btn play-button">
-          <i className="fa fa-play"></i>
-        </button>
+        <div className="section-content-container">
+          <button className="btn play-button">
+            <i className="fa fa-play"></i>
+          </button>
+          <h2 className="section-headings">Learn / Code / Create</h2>
+          <p className="section-subheadings">Blockchain * Apps * Games * Web</p>
+        </div>
       </div>
     </section>
   );
