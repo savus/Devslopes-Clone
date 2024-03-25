4eccd7fad362773478e72f3f1a1aa97998d9e33a
Savus <justin.fuzzygreenstuff@gmail.com>
2024-03-25 06:29:54 -0600

    begin work on VideoSection component, add devslopes video image and play button

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { TopHeader } from "./Components/TopHeader";
 import "./css/main.css";
 import "./css/theme.css";
 import "./css/responsive.css";
+import { VideoSection } from "./Components/VideoSection";
 
 function App() {
   return (
     <>
       <MainSection>
         <TopHeader />
+        <VideoSection />
       </MainSection>
     </>
   );
---
+++ src/Components/VideoSection.tsx
@@ -0,0 +1,12 @@
+import "../css/video-section.css";
+export const VideoSection = () => {
+  return (
+    <section className="video-section">
+      <div className="video-container">
+        <button className="btn play-button">
+          <i className="fa fa-play"></i>
+        </button>
+      </div>
+    </section>
+  );
+};
