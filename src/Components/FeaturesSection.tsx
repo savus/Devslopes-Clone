5de323c41c26ec6e47a60a87babf0fc5cefca857
Savus <justin.fuzzygreenstuff@gmail.com>
2024-04-16 00:33:41 -0600

    finish building and styling features-intro section with mobile responsive styles and give page header a fixed position

--- src/Components/FeaturesSection.tsx
+++ src/Components/FeaturesSection.tsx
@@ -4,27 +4,37 @@ import techLogo from "../assets/images/tech-logo.png";
 export const FeaturesSection = () => {
   return (
     <section className="features-section container container-lg">
-      <article className="features-header">
-        <h2 className="features-heading">Engaged Hands-on Learning</h2>
-        <h4 className="features-subheading">Professional Developer Training</h4>
-        <p>
-          Build real applications. Learn the frameworks and languages needed to
-          build professional web applications.
-        </p>
-        <p>
-          Devslopes brings guided learning in the most effective, engaging, and
-          fun way possible.
-        </p>
-        <img
-          src={mobile}
-          alt="mobile-devices-image"
-          className="mobile-devices"
-        />
-        <img
-          src={techLogo}
-          alt="development-languages-icons"
-          className="development-languages"
-        />
+      <article className="features-intro container container-md">
+        <div className="headings-container">
+          <h2 className="features-heading">Engaged Hands-on Learning</h2>
+          <h3 className="features-subheading">
+            Professional Developer Training
+          </h3>
+        </div>
+        <div className="intro-body">
+          <div className="intro-content">
+            <p>
+              Build real applications. Learn the frameworks and languages needed
+              to build professional web applications.
+            </p>
+            <p>
+              Devslopes brings guided learning in the most effective, engaging,
+              and fun way possible.
+            </p>
+          </div>
+          <div className="intro-images">
+            <img
+              src={mobile}
+              alt="mobile-devices-image"
+              className="mobile-devices"
+            />
+            <img
+              src={techLogo}
+              alt="development-languages-icons"
+              className="development-languages"
+            />
+          </div>
+        </div>
       </article>
     </section>
   );
